ff093b7ba4df70ca95d95fed46b1aacef4b80fb9
jack86125 <jack86125@users.noreply.github.com>
2026-08-19 22:33:18 +0800

    📝 更新博客内容 (08-19 22:33)

--- XHBlogs/data/friends.ts
+++ XHBlogs/data/friends.ts
@@ -1,4 +1,13 @@
 // 🛡️ 本文件由 XingHuiSama 控制台自动生成
 export interface Friend { id: string; name: string; url: string; description: string; avatar: string; themeColor: string; }
 
-export const friendsData: Friend[] = [];
+export const friendsData: Friend[] = [
+  {
+    "id": "friend_1787149943881",
+    "name": "安知鱼",
+    "url": "https://blog.anheyu.com/",
+    "avatar": "https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg",
+    "description": "生活明朗，万物可爱",
+    "themeColor": "#6366f1"
+  }
+];
